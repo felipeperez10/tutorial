8a810c5d6cf6e6526e349257a59ad84667e13b73
Gersain Galindez <gersain@ayte.co>
2026-06-13 18:51:04 -0500

    [REFACTOR] update files last
    t       [FIX] update files last t

--- app/dashboard/products/page.tsx
+++ app/dashboard/products/page.tsx
@@ -17,7 +17,7 @@ export default async function ProductsPage({
   return (
     <div className="w-full">
       <div className="flex w-full items-center justify-between">
-        <h1 className={`${lusitana.className} text-2xl`}>Products</h1>
+        <h1 className={`${lusitana.className} text-2xl`}>PRODUCTOS</h1>
       </div>
 
       <div className="mt-4 flex items-center justify-between gap-2 md:mt-8">
